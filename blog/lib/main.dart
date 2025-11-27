4065b68f03121506d954fc09674d4b3faff04681
Jona T. Feucht <14951074+jonafeucht@users.noreply.github.com>
2025-11-27 17:51:54 +0100

    1.4.3

--- blog/lib/main.dart
+++ blog/lib/main.dart
@@ -42,6 +42,7 @@ void main() {
               SidebarGroup(
                 title: 'Blog Posts',
                 links: [
+                  SidebarLink(text: "Release 1.4.3", href: '/blog/release-1-4-3'),
                   SidebarLink(text: "Release 1.4", href: '/blog/release-1-4-0'),
                   SidebarLink(text: "Release 1.3.20", href: '/blog/release-1-3-20'),
                   SidebarLink(text: "Release 1.3.14", href: '/blog/release-1-3-14'),
